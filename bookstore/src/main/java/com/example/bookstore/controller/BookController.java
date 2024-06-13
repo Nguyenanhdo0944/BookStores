27fa106027cc9e6713fd27cb25ffd85f599b9758
Nguyenanhdo0944 <153740466+Nguyenanhdo0944@users.noreply.github.com>
2024-06-13 20:27:37 +0700

    Initial commit

---
+++ bookstore/src/main/java/com/example/bookstore/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.example.bookstore.controller;
+
+import com.example.bookstore.entity.Book;
+import com.example.bookstore.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public String showAllBooks(Model model) {
+        List<Book> books = bookService.getAllBooks();
+        model.addAttribute("books", books);
+        return "book/list";
+    }
+}
